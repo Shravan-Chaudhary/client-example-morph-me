42397ee012b6f230e265382df064bef526028efd
Shravan <shravansinghchaudhary@gmail.com>
2024-11-21 01:30:39 +0530

    fix cookie delet

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -5,7 +5,14 @@ import { cookies } from 'next/headers'
 const logout = async () => {
   const cookieStore = await cookies()
   if (cookieStore.has('accessToken')) {
-    cookieStore.delete('accessToken')
+    cookieStore.delete({
+      name: 'accessToken',
+      path: '/',
+      domain: 'morph-me.vercel.app', // Same domain you used when setting
+      secure: true,
+      httpOnly: true,
+      sameSite: 'none',
+    })
   }
 }
 

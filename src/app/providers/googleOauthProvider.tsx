bf2893d640e0e32aaf3cf837abc2d119f5aa42c4
Shravan <shravansinghchaudhary@gmail.com>
2024-11-20 22:23:12 +0530

    update client id

--- src/app/providers/googleOauthProvider.tsx
+++ src/app/providers/googleOauthProvider.tsx
@@ -3,8 +3,11 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 export function OAuthProvider({ children }: { children: React.ReactNode }) {
+  if (!process.env.NEXT_PUBLIC_CLIENT_ID) {
+    throw new Error('Missing NEXT_PUBLIC_CLIENT_ID')
+  }
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID!}>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID}>
       {children}
     </GoogleOAuthProvider>
   )

ac9df590a00defa3195034755b5dae4711f5e175
Shravan <shravansinghchaudhary@gmail.com>
2024-11-21 01:38:25 +0530

    morph and upload headers

--- src/app/morph/components/upload.tsx
+++ src/app/morph/components/upload.tsx
@@ -75,6 +75,10 @@ export default function Upload({ onUploadComplete, onDelete }: UploadProps) {
         {
           method: 'POST',
           body: formData,
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          },
         },
       )
 
